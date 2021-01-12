1e3fad2f66864e803cc3fc717e50a6db2b51d3f0
blusanders <blusanders@yahoo.com>
2021-01-12 15:01:26 -0600

    Added fish array with current 4 fish

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,41 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+const fishCollection = [
+    {
+        name: "Nemo",
+        species:"Amphiprioninae",
+        location:"Great Barrier Reef",
+        size:"2-3in.",
+        food: "bananas",
+    }
+
+    {
+        name: "Marlin",
+        species:"Amphiprioninae",
+        location:"Great Barrier Reef",
+        size:"2-3in.",
+        food: "bananas",
+    }
+
+    {
+        name: "Chopper",
+        species:"Betta Splendens",
+        location:"Cambodia",
+        size:"1-5in.",
+        food: "plankton",
+    }
+
+    {
+        name: "Sunny",
+        species:"Actinopterygii",
+        location:"England's southwestern coast",
+        size:"6ft.",
+        food: "plankton",
+    }
+
+]
+
