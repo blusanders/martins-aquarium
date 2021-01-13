971096c70763c92ac057c1ccb9516bb6a291a785
blusanders <blusanders@yahoo.com>
2021-01-13 11:12:01 -0600

    Final final cleaned up code

--- scripts/fishList.js
+++ scripts/fishList.js
@@ -1,24 +1,21 @@
-/**
- *  FishList which renders individual fish objects as HTML
- */
+//render fish list
 
 import { useFish } from './FishDataProvider.js';
 import { Fish } from "./Fish.js";
 
+// find first element of class .containerLeft__fishList
+    // useFish provides the array of fish data
+    // loop through the fish data
+    // each fish gets returned formatted with HTML via the Fish function
+    // writes out HTML in between the class element via the query selector
+
 export const FishList = () => {
 
-    // find first element of class .containerLeft__fishList
-    // useFish provides the array of fish data
     const contentElement = document.querySelector(".containerLeft__fishList");
     const fishes = useFish()
 
-    //loop through the fish data and write fish element with HTML from fish.js
     for (const fish of fishes){
-
-        //each fish gets returned formatted with HTML via the Fish function
         let fishHTML = Fish(fish)
-
-        //write out the HTML in between the class element from the query selector
         contentElement.innerHTML += fishHTML;
     }
 
